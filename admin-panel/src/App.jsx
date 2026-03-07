2fe4456b484358ecfebe13c3cb2620dbb08b9fec
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-03-08 03:35:02 +0700

    update Dashboard

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,14 +1,11 @@
+import DashBoard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div style={{ display: "flex" }}>
       <Sidebar />
-
-      <div style={{ padding: "20px" }}>
-        <h1>Dashboard</h1>
-        <p>Welcome to admin panel</p>
-      </div>
+      <DashBoard />
     </div>
   );
 }
---
+++ admin-panel/src/components/Dashboard.jsx
@@ -0,0 +1,9 @@
+function DashBoard() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Admin</h1>
+      <p>Welcome to admin panel</p>
+    </div>
+  );
+}
+export default DashBoard;
