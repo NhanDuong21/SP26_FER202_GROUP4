439167cbf486c1fa6498a4ea4f30a672a3214d4a
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-03-12 02:47:41 +0700

    add app jsx using toaser improve UI

--- g4-fashion-admin/src/App.jsx
+++ g4-fashion-admin/src/App.jsx
@@ -1,7 +1,24 @@
 import AppRoutes from "./routes/AppRoutes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 2500,
+          style: {
+            borderRadius: "12px",
+            background: "#fff",
+            color: "#0f172a",
+            border: "1px solid #e2e8f0",
+          },
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
