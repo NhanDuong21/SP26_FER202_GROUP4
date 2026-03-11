60a74677ad4b6a368950ddc1b4936ce06e017201
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-03-12 02:47:06 +0700

    added util format slug

---
+++ g4-fashion-admin/src/utils/slugify.js
@@ -0,0 +1,17 @@
+/**
+ * util sử dụng:
+ * - bỏ dấu tiếng việt
+ * - chuyển chữ thường
+ * - thay khoảng trắng thành '-'
+ */
+export const slugify = (text) => {
+  return text
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/[^a-z0-9\s-]/g, "")
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+};
