a427e1397356f30ad18160df39d08ed55bf2900d
Le Kiet <kietlgce180993@fpt.edu.vn>
2026-03-16 00:43:58 +0700

    feat(api): create messageService for message CRUD operations

--- g4-fashion-admin/src/services/messageService.js
+++ g4-fashion-admin/src/services/messageService.js
@@ -1,6 +1,13 @@
-const API_URL = "http://localhost:3001/messages";
+import axiosClient from "./axiosClient";
 
-export const getMessages = async () => {
-  const res = await fetch(API_URL);
-  return res.json();
+export const messageService = {
+  getAll: () => axiosClient.get("/messages"),
+
+  getById: (id) => axiosClient.get(`/messages/${id}`),
+
+  create: (data) => axiosClient.post("/messages", data),
+
+  reply: (id, data) => axiosClient.put(`/messages/${id}`, data),
+
+  remove: (id) => axiosClient.delete(`/messages/${id}`),
 };
